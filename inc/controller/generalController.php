e410b6269a5c1887eb5481897b05faf9a1c2fdc3
Robin Pierrot <robin.pierrot@gmail.com>
2015-05-20 18:15:46 +0200

    Integration de l'index

---
+++ inc/controller/generalController.php
@@ -0,0 +1,13 @@
+<?php
+namespace controller;
+
+class generalController {
+
+	public function __construct () {
+		
+    }
+
+    public function genererSquelette($contenu, $meta = false, $article = null){
+    	global $twig;
+    	global $dev;
+    }
--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 
 include "inc/php/config.php";
 
-$i = new \classe\Image();
-$i->set('texte', 'Sale batard de fils de pute je vais niquer ta maman, ta grand-mère et aussi tout tes petits rejetons sale pd de mes deux');
-$i->set('pseudo', 'Je te love Jeanne d\'Arc');
-// $i->add();
-// $file = $i->generate();
+// $i = new \classe\Image();
+// $i->set('texte', 'Sale batard de fils de pute je vais niquer ta maman, ta grand-mère et aussi tout tes petits rejetons sale pd de mes deux');
+// $i->set('pseudo', 'Je te love Jeanne d\'Arc');
+// // $i->add();
+// // $file = $i->generate();
 
-// echo '<img src="'.$file.'" />';
+// // echo '<img src="'.$file.'" />';
 
-var_dump($i->likeKitten(1));
+// var_dump($i->likeKitten(1));
+
+// $pageController = new \controller\generalController();
+// echo $pageController->genererIndex();
+
+?>
