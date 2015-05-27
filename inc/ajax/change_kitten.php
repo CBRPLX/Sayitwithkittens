c208e9184100ecfa90b6bd888c760e7de1b92b79
Robin Pierrot <robin.pierrot@gmail.com>
2015-05-27 22:37:05 +0200

    Css

--- inc/ajax/change_kitten.php
+++ inc/ajax/change_kitten.php
@@ -2,9 +2,6 @@
 require "inc/php/config.php";
 
 $i = new \classe\Image();
-// while ($_POST['bg'] == $new_bg = $i->chooseKitten()) {
-	
-// }
 $nb_kitten = explode(".", explode("_", $_POST['bg'])[1])[0];
 $new_bg = $i->chooseKitten($nb_kitten);
 echo $new_bg;
