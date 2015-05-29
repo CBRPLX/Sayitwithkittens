d542fafb79249a57c000825adb9fefbd7f30f59a
Robin Pierrot <robin.pierrot@gmail.com>
2015-05-29 23:17:41 +0200

    Adding the newsletter

---
+++ inc/classe/Newsletter.php
@@ -0,0 +1,53 @@
+<?php
+namespace classe;
+
+class Newsletter{
+
+	private $id_newsletter;
+	private $nom;
+	private $email;
+	private $date_inscription;
+
+	public function __construct () {
+		
+    }
+
+    public function get($attr){
+    	return $this->$attr;
+    }
+
+    public function set($attr, $value){
+    	$this->$attr = $value;
+    }
+
+    public function add($nom, $email){
+        global $pdo;
+
+        $sql = "SELECT * FROM kitten_newsletter WHERE email = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array($email));
+
+        if($stmt->rowCount() == 0){
+            $sql = "INSERT INTO kitten_newsletter (nom, email, date_inscription) 
+                    VALUES (?,?,?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(array($nom, $email, time()));
+            if($stmt->rowCount() > 0){                
+                // $pageController = new \controller\emailController();
+                // $contenu = $pageController->genererInscriptionNews($_POST["nom"]);
+                
+                // $destinataire = ucfirst(strtolower($_POST["nom"]))." <".$_POST["email"].">";
+                // $pageController->envoyerEmail($destinataire, "Inscription à la newsletter cbrplx.io", $contenu);
+
+                // $contenu = $pageController->genererNouvelleInscription($_POST["nom"], $_POST["email"]);
+                // $pageController->envoyerEmail('cbrplx.io <robin.pierrot@gmail.com>', "Nouvelle inscription à la newsletter cbrplx.io", $contenu);
+
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
