71b181b0d4c1fdd7de45cdce53cfc7ce798ab2d6
Robin Pierrot <robin.pierrot@gmail.com>
2015-12-11 23:01:01 +0100

    header mail

--- inc/controller/emailController.php
+++ inc/controller/emailController.php
@@ -17,12 +17,12 @@ class emailController {
     public function envoyerEmail($destinataire, $sujet, $texte) {
         global $pdo;
 
-        $headers = "MIME-Version: 1.0 \n";
-        $headers .= "Content-Transfer-Encoding: 8bit \n";
-        $headers .= "Content-type: text/html; charset=UTF-8 \n";
-        $headers .= "From: Say it with kittens <hello@sayitwithkittens.io>  \n";
-        $headers .= "Bcc: Say it with kittens <cyberplix@gmail.com>  \n";
-        $headers .="Reply-To: Say it with kittens <hello@sayitwithkittens.io> \n";
+        $headers = "MIME-Version: 1.0 \r\n";
+        $headers .= "Content-Transfer-Encoding: 8bit \r\n";
+        $headers .= "Content-type: text/html; charset=UTF-8 \r\n";
+        $headers .= "From: Say it with kittens <hello@sayitwithkittens.io>  \r\n";
+        $headers .= "Bcc: Say it with kittens <cyberplix@gmail.com>  \r\n";
+        $headers .= "Reply-To: Say it with kittens <hello@sayitwithkittens.io> \r\n";
 
         $res = mail($destinataire, $sujet, $texte, $headers, "-f hello@sayitwithkittens.io");
         return $res;
