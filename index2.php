0dfc750aa049aa9cfd10ed9baa68c10597bb37ff
Robin Pierrot <robin.pierrot@gmail.com>
2015-12-13 20:16:10 +0100

    ok

--- index2.php
+++ index2.php
@@ -3,43 +3,12 @@
 include 'inc/php/config.php';
 
 ini_set('display_error', '1');
-error_reporting(E_ERROR | E_PARSE);
+error_reporting(-1);
 
-// $e = new \controller\emailController();
-// echo $e->genererInscriptionNews("Paul");
 
-// $from = "<hello@sayitwithkittens.io>";
-// $to = "<cyberplix@gmail.com>";
-
-// $subject = "Hi!";
-// $body = "Hi,\n\nHow are you?";
-
-// $host = "ssl://smtp.gmail.com";
-// $port = "465";
-// $username = "hello@sayitwithkittens.io";
-// $password = "KAC000762";
-
-// $headers = array ('From' => $from,
-//   'To' => $to,
-//   'Subject' => $subject);
-
-// $smtp = Mail::factory('smtp',
-//   array ('host' => $host,
-//     'port' => $port,
-//     'auth' => true,
-//     'username' => $username,
-//     'password' => $password));
-
-// $mail = $smtp->send($to, $headers, $body);
-
-// if (PEAR::isError($mail)) {
-//   echo("<p>" . $mail->getMessage() . "</p>");
-//  } else {
-//   echo("<p>Message successfully sent!</p>");
-//  }
 echo "ok";
 
-require '/vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 
 echo "ko";
  
